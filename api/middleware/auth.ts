0fb31392459c8500c8a4887b7577679cf4abbbd0
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2025-01-13 15:27:54 +0600

    auth middleware added

---
+++ api/middleware/auth.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from 'express';
+
+import User, { Fields as UserFields } from '../models/User';
+
+export interface RequestWithUser extends Request {
+  user: UserFields;
+}
+
+const auth = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+  const token = req.get('Authorization');
+
+  try {
+    const user = await User.findOne({ token });
+
+    if (!user) {
+      return void res.status(401).send({ error: 'Invalid token.' });
+    }
+
+    req.user = user;
+    next();
+  } catch (e) {
+    if (e instanceof Error) {
+      res.status(400).send({ error: e.message });
+    } else {
+      console.log(e);
+      res.status(400).send({ error: 'Unknown error. The administrator will be notified.' });
+    }
+  }
+};
+
+export default auth;
